d4dc8ae89ac6fc181a4aeeda5e47baffd947951b
mjuanfe <juanfe.monfort@gmail.com>
2022-09-20 20:25:52 +0200

    20-09
    Up Septiembre

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,9 +32,11 @@ module.exports = {
       resolve: 'gatsby-source-google-spreadsheets',
       options: {
         spreadsheetId: '13hD0bH9yILeil2roPC4yMXKwLNdIGjylqAj_BYObhRw',
-        apiKey: '64caa30bb496a28402a7f0e86197cae5d0153c8b',
-        // Or
-        credentials: require('./credentials.json')
+        apiKey: 'AIzaSyAIbEUOmzKG26oarbHB9bXKTLFkYtb1rsw',
+        // Or 64caa30bb496a28402a7f0e86197cae5d0153c8b antigua  64caa30bb496a28402a7f0e86197cae5d0153c8b
+        // https://docs.google.com/spreadsheets/d/e/2PACX-1vQKsp51B4uw6CiEclEk8aMPcA4LlR-IwGtv_uWK3ACHgRPAXyJlAkckm-k0_ojRC07V2pTZnRb_Qx6O/pubhtml
+        // https://docs.google.com/spreadsheets/d/13hD0bH9yILeil2roPC4yMXKwLNdIGjylqAj_BYObhRw/edit?usp=sharing
+       // credentials: require('./credentials.json')
       }
     },
     {
